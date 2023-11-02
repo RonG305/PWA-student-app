b78e655d695d10febf3811f955e9fd81cbbe14eb
RonG305 <mutiaronald138@gmail.com>
2023-11-02 13:29:40 +0300

    changes

--- API/frontend/src/sw.js
+++ API/frontend/src/sw.js
@@ -35,42 +35,4 @@ self.addEventListener('fetch', (event) => {
   );
 });
 
-self.addEventListener('sync', (event) => {
-  if (event.tag === 'syncData') {
-    event.waitUntil(syncData());
-  }
-});
-
-async function syncData() {
-  // Fetch and synchronize data with the server
-  const offlineData = JSON.parse(localStorage.getItem('offlineData'));
-
-  if (offlineData) {
-    try {
-      const response = await fetch('http://localhost/api/students', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(offlineData),
-      });
-
-      if (response.ok) {
-        // Data synchronization successful
-        localStorage.removeItem('offlineData');
-      } else {
-        console.error('Data synchronization failed:', response.statusText);
-      }
-    } catch (error) {
-      console.error('Error during data synchronization:', error);
-    }
-  }
-}
-
-
-// Listen for the online event and trigger data synchronization
-window.addEventListener('online', function () {
-  syncData(); // Synchronize data when the network connection is established
-  console.log('app is online')
-});
 
